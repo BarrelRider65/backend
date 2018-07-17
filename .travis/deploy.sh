43d175fc81795652786284941036c1bebf775e63
Haldir65 <mjw090608@gmail.com>
2018-07-17 03:30:26 -0400

    prepare for auto deployment

---
+++ .travis/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+echo `which node`
+node deploy.js
+echo "end of deploy process"
+
+git status
+git add .
+git commit -m "add auto commit file"
+git push
+
+echo "end of run scripts"
