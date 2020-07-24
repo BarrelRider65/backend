8d6d183a246eeb599b4941dcfc5ced050026dcb4
BarrelRider65 <862675121@qq.com>
2020-07-24 19:23:34 +0800

    renaming all images

--- rename.sh
+++ rename.sh
@@ -2,20 +2,24 @@
 FORMAT_JPG="jpg"
 FORMAT_JPEG="jpeg"
 index=1
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 dir=$(eval pwd)/pics
 # ALLIMGES=$(ls $dir | grep  ".$FORMAT_JPEG\|.$FORMAT_JPG")
 ALLIMGES=$(ls $dir)
 for file in $ALLIMGES
         do
-        name=img_${index}.jpg
-        echo renaming $dir/$file to  $dir/$name
+        name=img_${index}.webp
+        # echo renaming $dir/$file to  $dir/$name
+        # file  $dir/$file
         # mv $dir/$file $dir/$name
         ((index++))
         # name=$(ls $file | cut -d. -f1)
         # echo ${name}
-        #  mv $dir/$file ${dir}/$name
+        mv $dir/$file ${dir}/$name
         done
 echo "renaming $index image files =====> x.jpg done!"
+IFS=$SAVEIFS
 
 
 # # rename files using for loops
